256317b87a617436794be5e1b5440305d8872b1b
Matthew Quinn <matthew.quinn@libertymutual.com>
2021-01-08 16:20:18 -0500

    feat: added initial site information

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,12 +8,9 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <h1>Matt Quinn</h1>
+    <p>Site is under construction</p>
+    <p>Come back soon</p>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>

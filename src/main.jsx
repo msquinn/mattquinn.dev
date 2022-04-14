c9521b749fe0cd5f4a2d699969be5b39f41aa820
Matt Quinn <matt.quinn@blockfi.com>
2022-04-14 11:07:48 -0400

    feat: convert site to vite

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { createRoot } from "react-dom/client"
+import App from "./App"
+import "./index.scss"
+
+const container = document.getElementById("root")
+
+const app = createRoot(container)
+
+app.render(<App />)
---
+++ vite.config.js
@@ -0,0 +1,43 @@
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import {
+  createStyleImportPlugin,
+  AndDesignVueResolve,
+  VantResolve,
+  ElementPlusResolve,
+  NutuiResolve,
+  AntdResolve,
+} from "vite-plugin-style-import"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+      },
+    },
+  },
+  plugins: [
+    react(),
+    createStyleImportPlugin({
+      resolves: [
+        AndDesignVueResolve(),
+        VantResolve(),
+        ElementPlusResolve(),
+        NutuiResolve(),
+        AntdResolve(),
+      ],
+      libs: [
+        // If you don’t have the resolve you need, you can write it directly in the lib, or you can provide us with PR
+        {
+          libraryName: "ant-design-vue",
+          esModule: true,
+          resolveStyle: name => {
+            return `ant-design-vue/es/${name}/style/index`
+          },
+        },
+      ],
+    }),
+  ],
+})
